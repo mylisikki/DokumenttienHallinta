031e7fd127f2d5d3daad60f51d300de75ab4efd6
mylisikki <40537617+mylisikki@users.noreply.github.com>
2018-06-24 15:04:08 +0300

    Hei Johanna

--- Dokumenttienhallinta/src/Dokumenttienhallinta.java
+++ Dokumenttienhallinta/src/Dokumenttienhallinta.java
@@ -1,5 +1,6 @@
 public class Dokumenttienhallinta {
     public static void main(String[] args) {
         System.out.println("Hei Mika!! :D *:");
+	System.out.println("Hei Johanna!! :D *:");
     }
 }
